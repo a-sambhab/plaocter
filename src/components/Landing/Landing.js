2ddf569bdc4a12dabf9aab813af018d15c6dfdc4
kumarshantanu01 <shantanuk280@gmail.com>
2021-09-26 03:50:08 +0530

    landing page added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,20 @@
 import React from "react";
+import './landing.css';
 
 const Landing=() =>{
     return(
         <>
             <div className="landing">
+           
                 <div className="landing_image">
-                    <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1632564967/plaocter/New_Project_5_k2ev61.svg" alt=""/>
+                    <img className="landing_image1" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1632564967/plaocter/New_Project_5_k2ev61.svg" alt=""/>
                 </div>
+
+                <div className="landing_text">
+                    <h1>PLAOCTER</h1>
+                    <h2>BECAUSE WE CARE ABOUT YOUR PLANTS</h2>
+                </div>
+        
             </div>
         </>
     )
