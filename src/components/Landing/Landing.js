6312f0ab87816b19fbf6ea79fc5ded00ee5af977
kumarshantanu01 <shantanuk280@gmail.com>
2021-09-26 02:55:51 +0530

    landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,9 @@ const Landing=() =>{
     return(
         <>
             <div className="landing">
-                
+                <div className="landing_image">
+                    <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1632564967/plaocter/New_Project_5_k2ev61.svg" alt=""/>
+                </div>
             </div>
         </>
     )
